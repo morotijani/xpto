61c18d21b95fbc07adae034ab409f10934d22914
Tijani Moro <tjhackx111@gmail.com>
2025-02-15 04:59:58 +0000

    intiial commit

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+    
+    // AUTO LOAD VENDOR FILES
+    require dirname(__DIR__)  . '/xpto/vendor/autoload.php';
+    
+    // LOAD DOTENV
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+define('BASEURL', $_SERVER['DOCUMENT_ROOT'] . '/xpto/');
+	
+define('PROOT', '/xpto/');
