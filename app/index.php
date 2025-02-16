f584ee8dc7a4550702a5b10e59b722d251288343
Tijani Moro <tjhackx111@gmail.com>
2025-02-16 04:58:16 +0000

    initial commit

--- app/index.php
+++ app/index.php
@@ -1,9 +1,56 @@
 <?php
     require ("../system/DatabaseConnector.php");
+    $newFont = "yes";
     include ("../head.php");
+    
 
 ?>
 
+    <header id="header" class="navbar navbar-expand-lg navbar-light navbar-end bg-white">
+        <div class="container">
+            <nav class="js-mega-menu navbar-nav-wrap">
+                <a class="navbar-brand" href="<?= PROOT; ?>app/" aria-label="Space">
+                    <img class="navbar-brand-logo" src="<?= PROOT; ?>assets/media/logo.svg" style="width: auto; height: 80px;" alt="Image Description">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-default">
+                        <i class="bi-list"></i>
+                    </span>
+                    <span class="navbar-toggler-toggled">
+                        <i class="bi-x"></i>
+                    </span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="hs-has-mega-menu nav-item">
+                            <a id="landingsMegaMenu" class="nav-link active" aria-current="page" href="#" role="button" aria-expanded="false"><i class="bi-twitter me-1"></i></a>
+                        </li>
+                        <li class="hs-has-mega-menu nav-item">
+                            <a id="landingsMegaMenu" class="nav-link active" aria-current="page" href="#" role="button" aria-expanded="false"><i class="bi-facebook me-1"></i></a>
+                        </li>
+                    
+                        <li class="nav-divider"></li>
+
+                        <li class="nav-item">
+                            <a class="js-animation-link btn btn-ghost-secondary btn-no-focus me-2 me-lg-0" href="<?= PROOT; ?>auth/login">Deposite</a>
+                            <a class="js-animation-link d-lg-none btn btn-primary" href="<?= PROOT; ?>auth/signup">
+                                <i class="bi-sign-turn-slight-right me-1"></i> Withdraw
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="js-animation-link d-none d-lg-inline-block btn btn-primary" href="<?= PROOT; ?>auth/signup">
+                                <i class="bi-sign-turn-slight-right me-1"></i> Withdraw
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    
 	<!-- ========== MAIN CONTENT ========== -->
 	<main id="content" role="main">
 
--- head.php
+++ head.php
@@ -9,12 +9,18 @@
     <title>XPTO</title>
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="<?= PROOT; ?>assets/media/logo.jpg">
+    <link rel="shortcut icon" href="<?= PROOT; ?>assets/media/logo.svg">
 
     <!-- Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Turret+Road:wght@200;300;400;500;700;800&display=swap" rel="stylesheet">
+    <?php if ($newFont == 'yes'): ?>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap" rel="stylesheet">
+    <?php else: ?>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Turret+Road:wght@200;300;400;500;700;800&display=swap" rel="stylesheet">
+    <?php endif; ?>
 
     <!-- CSS Implementing Plugins -->
     <link rel="stylesheet" href="<?= PROOT; ?>assets/css/vendor.min.css">
@@ -23,9 +29,15 @@
     <link rel="stylesheet" href="<?= PROOT; ?>assets/css/theme.min.css?v=1.0">
     <style>
         * {
-            font-family: "Turret Road", serif;
-            /* font-weight: 200; */
+            <?php if ($newFont == 'yes'): ?>
+            font-family: "Cutive Mono", serif;
+            font-weight: 400;
             font-style: normal;
+            <?php else: ?>
+                font-family: "Turret Road", serif;
+                /* font-weight: 200; */
+                font-style: normal;
+            <?php endif; ?>
         }
 
         .turret-road-extralight {
